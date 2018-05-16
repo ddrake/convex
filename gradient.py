615be1e2c45aad6c9fc92d4d67bcf59a45782d29
Dow Drake <dowdrake@msn.com>
2018-05-15 21:35:50 -0700

    initial commit

---
+++ gradient.py
@@ -0,0 +1,53 @@
+from numpy import *
+
+def f(x):
+    return x[0]**2 + x[0]*x[1] + x[1]**2 + x[0] - x[1]
+
+def grad_f(x):
+    gf = array([2*x[0]+x[1]+1,x[0]+2*x[1]-1])
+    return gf
+
+def gradient(max_gradf=1.0e-4, x0=[0,0], t=0.1):
+    x1s = []
+    x2s = []
+    fs = []
+    xk = x0
+    gfk = grad_f(xk)
+    gfk_n2 = gfk.dot(gfk)
+    while gfk_n2 > max_gradf:
+        gfk = grad_f(xk)
+        gfk_n2 = gfk.dot(gfk)
+        xk -= t*gfk
+        fk = f(xk)
+        x1s.append(xk[0])
+        x2s.append(xk[1])
+        fs.append(fk)
+    return gfk_n2, fs, x1s, x2s
+
+def nesterov(max_gradf=1.0e-4, x0=[0,0], t=0.1):
+    x1s = []
+    x2s = []
+    fs = []
+    gs = []
+    xk = x0
+    yk = x0
+    gfk = grad_f(xk)
+    gfk_n2 = gfk.dot(gfk)
+    tk = 1
+    while gfk_n2 > max_gradf:
+        gfk = grad_f(xk)
+        gfk_n2 = gfk.dot(gfk)
+        xk1 = yk - t*gfk
+        tk1 = (1.0 + sqrt(1.0 + 4.0*tk*tk))/2.0
+        g = (tk-1)/tk1
+        yk = xk + g*(xk1 - xk)
+        fk = f(xk)
+        x1s.append(xk1[0])
+        x2s.append(xk1[1])
+        gs.append(g)
+        fs.append(fk)
+        tk = tk1
+        xk = xk1
+    return fs, x1s, x2s, gs
+
+
